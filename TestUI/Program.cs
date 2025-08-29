16a28a9aa27ed3b32c0809f8f3177875699678b9
Vladimir Zimin <showmangta@gmail.com>
2025-08-29 18:01:58 +0800

    Добавьте файлы проекта.

---
+++ TestUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Графичекий интерфейс");
+        }
+    }
+}
