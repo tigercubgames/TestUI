a21d8dfdbd40a408c52593bbcfb5d60c9b71e20d
Vladimir Zimin <showmangta@gmail.com>
2025-08-29 18:33:05 +0800

    Изменение сообщения

--- TestUI/Program.cs
+++ TestUI/Program.cs
@@ -6,7 +6,7 @@ namespace TestUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git Графичекий интерфейс");
+            Console.WriteLine("Графичекий интерфейс");
         }
     }
 }
